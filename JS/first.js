dab934234f971b2bc6448fb4cd0218a9372b35c4
laki <garvitsolanki1111@gmail.com>
2024-10-22 16:14:54 +0530

    new

---
+++ JS/first.js
@@ -0,0 +1,54 @@
+// aretimetic operators
+// let a =5;
+// let b =9;
+/*console.log("a + b = ",a+b);
+console.log("a - b = ",a-b);
+console.log("a * b = ",a*b);
+console.log("a / b = ",a/b);
+console.log("a % b = ",a%b);
+console.log("a ** b = ",a**b);*/
+
+// Unary Operator
+
+// a++;
+// b--;
+// console.log(a);
+// console.log(b);
+
+
+// comparison operator
+a = 4;
+b = 4;
+// console.log(a);
+
+
+
+ let digit = 4000;
+ 
+
+ if (digit>9 && digit <99 ){
+    console.log("two digit number");  
+ }
+ else if (digit>99 && digit <999){
+    console.log("three digit number");  
+ }
+ else if (digit>999 && digit <9999 ){
+    console.log("four digit number");  
+ }
+ else if (digit>9999 && digit <99999 ){
+    console.log("five digit number");  
+ }
+else {
+    console.log("one digit")
+ }
+
+ 
+
+
+
+
+
+
+
+
+
